382de85c745724e594e5b4dcadcce52a1ac4feca
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-01-09 10:47:32 -0600

    Fixing

--- fcs-etl-0.3.2/runner-template.sh
+++ fcs-etl-0.3.2/runner-template.sh
@@ -48,10 +48,8 @@ echo "cytometerConfiguration: ${cytometerConfiguration}" >> inputs.txt
 echo "processControl: ${processControl}" >> inputs.txt
 echo "experimentalData: ${experimentalData}" >> inputs.txt
 echo "colorModelParameters: ${colorModelParameters}" >> inputs.txt
-
 echo "analysisParameters: ${analysisParameters}" >> input.txt
 echo "inputData: ${inputData}" >> input.txt
-echo "dummyInput: ${dummyInput}" >> input.txt
 
 OWD=$PWD
 # Predicted directory. Saem as inputData if not archive
