77c1369b0d83d3ab0aa8e9840b4e27845e55996b
Jed Singer <jed.singer@twosixlabs.com>
2018-02-13 02:13:53 +0000

    semicolons and tasbeconfig

--- src/analysis.py
+++ src/analysis.py
@@ -24,11 +24,12 @@ class Analysis:
       os.makedirs(folder_path)
 
     if 'point_clouds' in self.obj.get('additional_outputs', []) or 'bayesdb_files' in self.obj.get('additional_outputs', []):
-      self.octave.eval('TASBEConfig.set("makePointCloudFiles", true);')
+      self.octave.eval('TASBEConfig.set("flow.outputPointCloud", true);')
       folder = os.path.split(self.obj['output']['file'])[0]
+      print('\n\n++++++++\n' + folder + '\n+++++++++\n\n')
       self.octave.eval('TASBEConfig.set("flow.pointCloudPath","{}");'.format(folder))
     else:
-      self.octave.eval('TASBEConfig.set("makePointCloudFiles", false);')
+      self.octave.eval('TASBEConfig.set("flow.outputPointCloud", false);')
 
   def analyze(self):
     self.octave.eval('bins = BinSequence(0,0.1,10,\'log_bins\');');
--- src/experiment.py
+++ src/experiment.py
@@ -17,8 +17,8 @@ class Experiment:
     self.octave.eval('file_pairs = {}')
     i =1 
     for sample in self.obj['samples']:
-      self.octave.eval('file_pairs{{{0},1}} = \"{1}\"'.format(i,sample['sample']))
-      self.octave.eval('file_pairs{{{0},2}} = {{[\'{1}\'] }} '.format(i,sample['file']))
+      self.octave.eval('file_pairs{{{0},1}} = \"{1}\";'.format(i,sample['sample']))
+      self.octave.eval('file_pairs{{{0},2}} = {{[\'{1}\'] }};'.format(i,sample['file']))
       i+=1
 
     #self.octave.eval('file_pairs')
