9123cf365ac4d8fdb41e7903498c108e84ff0da1
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-03-13 11:33:40 -0500

    Typo in install_configure_cli

--- scripts/install_configure_cli.sh
+++ scripts/install_configure_cli.sh
@@ -16,6 +16,14 @@ curl -skL -O https://github.com/SD2E/sd2e-cli/raw/ci/sd2e-cloud-cli.tgz && \
   echo "export PATH=\$PATH:$HOME/sd2e-cloud-cli/bin" >> $HOME/.bash_profile
 
 source ~/.bashrc
+source ~/.bash_profile
+
+echo "$HOME"
+echo "$PATH"
+
+pwd
+
+ls -alth
 
 tenants-init -t "${_AGAVE_TENANT}" && \
 auth-tokens-create -S -u ${_AGAVE_USERNAME} -p ${_AGAVE_PASSWORD} -k ${_AGAVE_APIKEY} -s ${_AGAVE_APISECRET} && \
