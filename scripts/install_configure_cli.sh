99a991b61fda946d3d7504f1ca1bb220ee6496dc
Matt Vaughn <vaughn@tacc.utexas.edu>
2018-03-13 11:47:28 -0500

    Fix Travis PATH

--- scripts/install_configure_cli.sh
+++ scripts/install_configure_cli.sh
@@ -11,19 +11,7 @@
 curl -skL -O https://github.com/SD2E/sd2e-cli/raw/ci/sd2e-cloud-cli.tgz && \
   mv sd2e-cloud-cli.tgz $HOME/ && \
   cd $HOME && \
-  tar zxvf sd2e-cloud-cli.tgz && \
-  echo "export PATH=\$PATH:$HOME/sd2e-cloud-cli/bin" >> $HOME/.bashrc && \
-  echo "export PATH=\$PATH:$HOME/sd2e-cloud-cli/bin" >> $HOME/.bash_profile
-
-source ~/.bashrc
-source ~/.bash_profile
-
-echo "$HOME"
-echo "$PATH"
-
-pwd
-
-ls -alth
+  tar zxvf sd2e-cloud-cli.tgz
 
 tenants-init -t "${_AGAVE_TENANT}" && \
 auth-tokens-create -S -u ${_AGAVE_USERNAME} -p ${_AGAVE_PASSWORD} -k ${_AGAVE_APIKEY} -s ${_AGAVE_APISECRET} && \
